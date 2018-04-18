266fa8e5d8b5540df412a438c8225b78c6cbca48
bosan120 <mohumohu30025@gmail.com>
2018-04-18 18:01:57 +0900

    Create aws-init.sh

---
+++ aws-init.sh
@@ -0,0 +1,33 @@
+#!usr/bin/bash
+
+sudo yum update -y
+sudo yum upgrade -y
+
+sudo yum install -y \
+    git \
+    tmux \
+    zsh \
+    curl \
+    tree \
+    wget \
+    vim \
+    python-pip \
+    
+#zshをログインシェルにしたい時は/etc/passwdの中にあるec2-userの行の/bin/bashを/bin/zshにする
+#git clone https://github.com/bosan120/common-settings
+
+touch .zshrc 
+touch .vimrc
+touch .tmux.conf
+
+cd ~/common-settings/
+cp .zshrc ~/.zshrc
+cp .vimrc ~/.vimrc
+cp .tmux.conf ~/.tmux.conf
+
+cd ~
+git clone https://github.com/powerline/fonts.git --depth=1
+./install.sh
+cd ..
+rm -rf fonts
+pip install --user powerline-shell
